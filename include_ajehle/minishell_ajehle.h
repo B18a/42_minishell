644dcd5306243d38c10a5728d634d16d29cfedeb
Andreas Jehle <ajehle@2-D-4.42heilbronn.de>
2024-03-27 16:51:46 +0100

    test

--- include_ajehle/minishell_ajehle.h
+++ include_ajehle/minishell_ajehle.h
@@ -6,16 +6,13 @@
 /*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 16:36:10 by ajehle            #+#    #+#             */
-/*   Updated: 2024/03/27 16:43:26 by ajehle           ###   ########.fr       */
+/*   Updated: 2024/03/27 16:50:24 by ajehle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef MINISHELL_AJEHLE_H
 # define MINISHELL_AJEHLE_H
 
 # include "../include/minishell.h"
-
-
 
 #endif
--- include_psanger/minishell_psanger.h
+++ include_psanger/minishell_psanger.h
@@ -6,16 +6,13 @@
 /*   By: ajehle <ajehle@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 16:36:10 by ajehle            #+#    #+#             */
-/*   Updated: 2024/03/27 16:43:30 by ajehle           ###   ########.fr       */
+/*   Updated: 2024/03/27 16:50:22 by ajehle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef MINISHELL_PSANGER_H
 # define MINISHELL_PSANGER_H
 
 # include "../include/minishell.h"
-
-
 
 #endif
